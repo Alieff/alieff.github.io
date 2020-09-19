e93e43e0dac80ddb1e240032c3e414773c782fd8
Alief Aziz <serdralfs@gmail.com>
2020-09-19 23:59:45 +0700

    adjust styling

--- behaviour.js
+++ behaviour.js
@@ -22,33 +22,63 @@ let p2_batu = ( nilai_p2 == 0 )
 let p2_gunting = ( nilai_p2 == 1 )
 let p2_kertas = ( nilai_p2 == 2 )
 
+
+
 if ( p1_batu && p2_batu ) {
-  document.querySelector("#pesan_p1").innerHTML = 'game seri'
-  document.querySelector("#pesan_p2").innerHTML = 'game seri'
+  seri()
 } else if ( p1_batu && p2_gunting ){
-  document.querySelector("#pesan_p1").innerHTML = 'kamu menang'
-  document.querySelector("#pesan_p2").innerHTML = 'kamu kalah'
+  p1_menang()
 } else if ( p1_batu && p2_kertas ){
-  document.querySelector("#pesan_p1").innerHTML = 'kamu kalah'
-  document.querySelector("#pesan_p2").innerHTML = 'kamu menang'
+  p2_menang()
 } else if ( p1_gunting && p2_batu ) {
-  document.querySelector("#pesan_p1").innerHTML = 'kamu kalah'
-  document.querySelector("#pesan_p2").innerHTML = 'kamu menang'
+  p2_menang()
 } else if ( p1_gunting && p2_gunting ){
-    document.querySelector("#pesan_p1").innerHTML = 'game seri'
-  document.querySelector("#pesan_p2").innerHTML = 'game seri'
+  seri()
 } else if ( p1_gunting && p2_kertas ){
-  document.querySelector("#pesan_p1").innerHTML = 'kamu menang'
-  document.querySelector("#pesan_p2").innerHTML = 'kamu kalah'
+  p1_menang()
 } else if ( p1_kertas && p2_batu ) {
-  document.querySelector("#pesan_p1").innerHTML = 'kamu menang'
-  document.querySelector("#pesan_p2").innerHTML = 'kamu kalah'
+  p1_menang()
 } else if ( p1_kertas && p2_gunting ){
-  document.querySelector("#pesan_p1").innerHTML = 'kamu kalah'
-  document.querySelector("#pesan_p2").innerHTML = 'kamu menang'
+  p2_menang()
 } else if ( p1_kertas && p2_kertas ){
-  document.querySelector("#pesan_p1").innerHTML = 'game seri'
-  document.querySelector("#pesan_p2").innerHTML = 'game seri'
+  seri()
+}
+
+function p1_menang(){
+  let elemen_pesan_p1 = document.querySelector("#pesan_p1")
+  let elemen_pesan_p2 = document.querySelector("#pesan_p2")
+  elemen_pesan_p1.innerHTML = 'Kamu menang !'
+  elemen_pesan_p2.innerHTML = 'Kamu kalah'
+  clear(elemen_pesan_p1)
+  clear(elemen_pesan_p2)
+  elemen_pesan_p1.classList.add("menang")
+  elemen_pesan_p2.classList.add("kalah")
+}
+function p2_menang(){
+  let elemen_pesan_p1 = document.querySelector("#pesan_p1")
+  let elemen_pesan_p2 = document.querySelector("#pesan_p2")
+  document.querySelector("#pesan_p1").innerHTML = 'Kamu kalah'
+  document.querySelector("#pesan_p2").innerHTML = 'Kamu menang !'
+  clear(elemen_pesan_p1)
+  clear(elemen_pesan_p2)
+  elemen_pesan_p1.classList.add("kalah")
+  elemen_pesan_p2.classList.add("menang")
+
+}
+function seri(){
+  let elemen_pesan_p1 = document.querySelector("#pesan_p1")
+  let elemen_pesan_p2 = document.querySelector("#pesan_p2")
+  document.querySelector("#pesan_p1").innerHTML = 'Game seri'
+  document.querySelector("#pesan_p2").innerHTML = 'Game seri'
+  clear(elemen_pesan_p1)
+  clear(elemen_pesan_p2)
+  elemen_pesan_p1.classList.add("seri")
+  elemen_pesan_p2.classList.add("seri")
+}
+function clear(element){
+  element.classList.remove(".menang")
+  element.classList.remove(".kalah")
+  element.classList.remove(".seri")
 }
 
 
