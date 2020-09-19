58180df3454c1326e571e3f4ca808ab221cdb81f
Alief Aziz <serdralfs@gmail.com>
2020-09-20 00:29:17 +0700

    feat-adjust: minor adjustment

--- behaviour.js
+++ behaviour.js
@@ -11,6 +11,7 @@ let images_kanan = [
 let nilai_p1 = Math.floor( Math.random() * 3 ) // 0-0.999
 let nilai_p2 = Math.floor( Math.random() * 3 ) // 0-0.999
 
+// ubah gambar secara random
 document.querySelector( "#player1" ).setAttribute( "src", images_kiri[nilai_p1] )
 document.querySelector( "#player2" ).setAttribute( "src", images_kanan[nilai_p2] )
 
@@ -23,25 +24,12 @@ let p2_gunting = ( nilai_p2 == 1 )
 let p2_kertas = ( nilai_p2 == 2 )
 
 
-
-if ( p1_batu && p2_batu ) {
+if ( ( p1_batu && p2_batu ) || ( p1_kertas && p2_kertas ) || ( p1_gunting && p2_gunting ) ) {
   seri()
-} else if ( p1_batu && p2_gunting ){
+} else if ( ( p1_batu && p2_gunting ) || ( p1_gunting && p2_kertas ) || ( p1_kertas && p2_batu ) ){
   p1_menang()
-} else if ( p1_batu && p2_kertas ){
+} else if ( ( p1_batu && p2_kertas ) || ( p1_gunting && p2_batu ) || ( p1_kertas && p2_gunting ) ){
   p2_menang()
-} else if ( p1_gunting && p2_batu ) {
-  p2_menang()
-} else if ( p1_gunting && p2_gunting ){
-  seri()
-} else if ( p1_gunting && p2_kertas ){
-  p1_menang()
-} else if ( p1_kertas && p2_batu ) {
-  p1_menang()
-} else if ( p1_kertas && p2_gunting ){
-  p2_menang()
-} else if ( p1_kertas && p2_kertas ){
-  seri()
 }
 
 function p1_menang(){
@@ -63,7 +51,6 @@ function p2_menang(){
   clear(elemen_pesan_p2)
   elemen_pesan_p1.classList.add("kalah")
   elemen_pesan_p2.classList.add("menang")
-
 }
 function seri(){
   let elemen_pesan_p1 = document.querySelector("#pesan_p1")
